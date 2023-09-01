2a357d30f51845e9ec84cd182048c80cc6305ff2
Cristopher811 <cristcerpug@gmail.com>
2023-09-01 01:40:04 -0600

    walks almost done

--- problems/randomwalk.py
+++ problems/randomwalk.py
@@ -5,27 +5,20 @@ import matplotlib as mpl
 import numpy as np
 import random  
 
-def lstdigits(n):
-    n = str(n)
-    if '.' in n:
-      n = n.replace('.','')
-    digits = list(n)
-    digits = [int(i) for i in digits]
-    return digits
-
-
-def piwalk(n,m):
-    digits = lstdigits(n)
+def randomwalk(m):
+    rand = []
+    distance = [0]
+    step = 0
+    average = 0
+    countx = 0
+    county = 0
     k = m+1
     x = np.zeros(k)
     y = np.zeros(k)
-    countx = 0
-    county = 0
-    total_steps = 0
-    x[0] = countx
-    y[0] = county
-    distance = [0]
-
+    for i in range(0,m):
+        rand.append(random.randint(0,9)) 
+    x[0] = 0
+    y[0] = 0
     step_map = {
         0: (0, 0),
         1: (0, 0),
@@ -39,52 +32,54 @@ def piwalk(n,m):
         9: (0, -1),
     }
     for i in range(1,k):
-        dx,dy = step_map[digits[i-1]]
+        dx,dy = step_map[rand[i-1]]
         countx += dx
         county += dy
         x[i] = countx
         y[i] = county
-        if digits[i-1] != 1 and digits[i-1] != 0:
+        if rand[i-1] != 1 and rand[i-1] != 0:
+            step +=1
             distance.append(np.sqrt(x[i]**2+y[i]**2))
     # create a figure
     fig = plt.figure()
+    for i in range(0,len(distance)):
+        average += distance[i]
     # create a plot into the figure
+    average = average/step
+    print(step)
+    print(average)
     ax = fig.add_subplot(111)
-
-    ax.set_xlabel('Step')
-    ax.set_ylabel('Average distance')
-
     ax.plot(distance)
+    plt.axhline(y=average, color='r', linestyle='-')
+    plt.title("Average distance to the origin (d = {})".format(m))
+    ax.set_ylabel("Average distance to the origin")
+    ax.set_xlabel("Step")
     return x,y
 
-m = 10**3
 
-mp.dps = m
-n = mp.pi
+m = 10**6
 
-x,y = piwalk(n,m)
-
+x1,y1 = randomwalk(m)
 # create a figure
 fig = plt.figure()
 # create a plot into the figure
-ax = fig.add_subplot(111)
+ax1 = fig.add_subplot(111)
 
-colors = np.linspace(0, 1, len(x))
-
-# Plot the graph with a color gradient
-for i in range(1, len(x)):
-    ax.plot([x[i-1],x[i]], [y[i-1],y[i]], color=plt.cm.viridis(colors[i]))
-
+colors1 = np.linspace(0, 1, len(x1))
+# plot the data
+for i in range(1, len(x1)):
+    ax1.plot([x1[i-1],x1[i]], [y1[i-1],y1[i]], color=plt.cm.viridis(colors1[i]))
 
 # Add color bar with tick labels
 cmap = plt.cm.viridis
 norm = plt.Normalize(0, 1)
 sm = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
 sm.set_array([]) # Dummy array for color mapping
-cbar = plt.colorbar(sm, ax=ax)
+cbar = plt.colorbar(sm, ax=ax1)
 cbar.set_ticks([0, 1])
 cbar.set_ticklabels(['Beginning', 'End'])
 
-plt.title("Pi-walk")
+plt.title("Random-walk")
+
 plt.show()
 
